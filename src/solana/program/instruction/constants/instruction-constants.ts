86bbcc8e38ee2dc37d172cc457072e0d0b5b86c0
mfbz <michael.fabozzi@icloud.com>
2021-02-27 10:35:07 +0100

    Built the program on the devnet and added address to web app program id

--- src/solana/program/instruction/constants/instruction-constants.ts
+++ src/solana/program/instruction/constants/instruction-constants.ts
@@ -3,9 +3,8 @@ import { PublicKey } from '@solana/web3.js';
 import { struct, utf8 } from 'buffer-layout';
 import { AppInstructionLayoutType } from '../interfaces/instruction-schema';
 
-// TODOOOOOOOOOO: replace with the correct program id passed as input
-// I have to set a real IDDDDDDDD SO BUILD THE PROGRAM AND PASS IT
-export const APP_PROGRAM_ID = new PublicKey('AppkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+// App program id
+export const APP_PROGRAM_ID = new PublicKey('BJT7bwqcnXRiw9B96RiXWL4bKgHwXr4cPW8T8CTkzyNk');
 
 // Define the structure of each entry in app-instruction file
 export const APP_INSTRUCTION_LAYOUT: EnumLayout<AppInstructionLayoutType> = rustEnum([
